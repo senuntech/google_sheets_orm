f5851bb687a96bea500b063ed33fa2acde0bf76f
Cristian Paulo <cristianpaulo4@gmail.com>
2026-01-10 14:58:01 -0300

    chore: add class formula

---
+++ lib/src/formula.dart
@@ -0,0 +1,13 @@
+class Formula {
+  String formula;
+  String columns;
+  String sheet;
+  int row;
+
+  Formula({
+    required this.formula,
+    required this.columns,
+    required this.sheet,
+    required this.row,
+  });
+}
